d6682b5ecbdfa24ac53c1ba3b127ac2779bbbc31
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-10-19 12:14:35 +0100

    Add Ficha de trabalho 1

---
+++ 12º Ano/Módulo 6/Ficha de trabalho 1/Ficha de trabalho 1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ficha_de_trabalho_1
+{
+    class Program
+    {
+        struct Avião
+        {
+            public string Nome;
+            public int Número;
+
+            public Avião(string Nome, int Número)
+            {
+                this.Nome = Nome;
+                this.Número = Número;
+            }
+        }
+
+        static Queue<Avião> filaDescolagem;
+
+        static void Main(string[] args)
+        {
+            filaDescolagem = new Queue<Avião>();
+
+            filaDescolagem.Enqueue(new Avião("Boeing 747", 747));
+            filaDescolagem.Enqueue(new Avião("Boeing 777", 777));
+            filaDescolagem.Enqueue(new Avião("Airbus A380", 380));
+
+            Console.WriteLine(filaDescolagem.Peek().Nome + " Autorizado a Descolar");
+            filaDescolagem.Dequeue();
+
+            filaDescolagem.Enqueue(new Avião("Boeing 666", 666));
+
+            Console.WriteLine("Fila de Descolagem:");
+            foreach (Avião avião in filaDescolagem)
+            {
+                Console.WriteLine("\t{0}: {1}", avião.Nome, avião.Número);
+            }
+
+            Avião primeiro = filaDescolagem.Peek();
+            Console.WriteLine("Primeiro avião da fila é um {0}, número {1}", primeiro.Nome, primeiro.Número);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 12º Ano/Módulo 6/Ficha de trabalho 1/README.md
@@ -0,0 +1,13 @@
+# Ficha de trabalho 1
+Escreva um programa que simule o controle de uma pista de descolagem de aviões em um aeroporto. 
+Neste programa, o utilizador deve ser capaz de realizar as seguintes tarefas: 
+
+a) Listar o número de aviões que aguardam na fila de descolagem;
+
+b) Autorizar a descolagem do primeiro avião da fila;
+
+c) Adicionar um avião à fila de espera;
+
+d) Listar todos os aviões na fila de espera;
+
+e) Listar as características do primeiro avião da fila. Considere que os aviões possuem um nome e um número inteiro como identificador. Adicione outras características conforme achar necessário.
