9680b14450a521f8f9bdecca0809cd377570fa35
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-10-30 15:48:23 +0000

    Add Ficha de Trabalho 3

---
+++ 12º Ano/Módulo 6/Ficha de trabalho 3/Ficha de trabalho 3/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ficha_de_trabalho_3
+{
+    class Program
+    {
+        struct TemperaturaCidade
+        {
+            public string cidade;
+            public double temperatura;
+
+            public TemperaturaCidade(string cidade, double temperatura)
+            {
+                this.cidade = cidade;
+                this.temperatura = temperatura;
+            }
+        }
+
+        static Queue<TemperaturaCidade> temperaturaCidade;
+
+        static void Main(string[] args)
+        {
+            CriarFila();
+            MainMenu();
+        }
+
+        static void MainMenu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("a) Apagar Registos.");
+            Console.WriteLine("b) Verificar se registos estão vazios.");
+            Console.WriteLine("c) Inserir novo registo.");
+            Console.WriteLine("d) Apagar ultimo registo.");
+            Console.WriteLine("e) Obter temperatura máxima.");
+
+            Console.Write("Selecione uma opção:");
+            char Opção = Console.ReadLine().ToLower()[0];
+
+            if (Opção >= 'a' & Opção <= 'e')
+            {
+                Console.Clear();
+
+                switch (Opção)
+                {
+                    case 'a':
+                        CriarFila();
+                        break;
+                    case 'b':
+                        IsFilaVazia();
+                        break;
+                    case 'c':
+                        inserirRegisto();
+                        break;
+                    case 'd':
+                        removerRegisto();
+                        break;
+                    case 'e':
+                        getTemperaturaMaxima();
+                        break;
+                }
+                Console.ReadLine();
+            }
+            MainMenu();
+        }
+
+        static void CriarFila()
+        {
+            temperaturaCidade = new Queue<TemperaturaCidade>();
+            Console.WriteLine("Registos apagados com successo!");
+        }
+
+        static void IsFilaVazia()
+        {
+            Console.WriteLine(((temperaturaCidade.Count == 0) ? "Não " : "") + "Existem Registos!");
+        }
+
+        static void inserirRegisto()
+        {
+            Console.Write("Cidade: ");
+            string cidade = Console.ReadLine();
+            Console.Write("Temperatura: ");
+            double temperatura = double.Parse(Console.ReadLine());
+            temperaturaCidade.Enqueue(new TemperaturaCidade(cidade, temperatura));
+        }
+
+        static void removerRegisto()
+        {
+            temperaturaCidade.Dequeue();
+            Console.WriteLine("Registo removido!");
+        }
+
+        static void getTemperaturaMaxima()
+        {
+            if (temperaturaCidade.Count > 0)
+            {
+                TemperaturaCidade max = temperaturaCidade.Peek();
+                foreach (TemperaturaCidade temp in temperaturaCidade)
+                {
+                    if (temp.temperatura > max.temperatura)
+                    {
+                        max = temp;
+                    }
+                }
+                Console.WriteLine("Temperatura máxima foi de {0}ºC em {1}", max.temperatura, max.cidade);
+            }
+            else
+            {
+                Console.WriteLine("Não existem registos");
+            }
+        }
+    }
+}
---
+++ 12º Ano/Módulo 6/Ficha de trabalho 3/README.md
@@ -0,0 +1,18 @@
+# Ficha de trabalho 3
+Elabore um programa que contemple as seguintes informações utilizando memória dinâmica:
+
+a. Crie uma estrutura que permita registar o nome de uma cidade (máx. 20 caracteres) e as temperaturas médias (ao longo de todos os meses do ano) do tipo inteiro;
+
+b. Crie um procedimento para criar uma FILA de dados vazia;
+
+c. Crie uma função para verificar se a FILA de dados está, ou não, vazia;
+
+d. Crie um procedimento para inserir um novo registo na FILA;
+
+e. Crie um procedimento para remover um elemento da FILA;
+
+f. Crie uma função que permita verificar a temperatura máxima do ano registado;
+
+g. Crie uma função/procedimento que permita verificar qual a cidade e o mês onde se obteve a maior temperatura do ano;
+
+h. Elabore um menu que permita chamar as funções e procedimentos acima mencionados, de forma a inserir as várias temperaturas de várias cidades de acordo com o numero de cidades assinalados pelo utilizador.
